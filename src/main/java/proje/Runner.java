cdd779dccb0d56b79d9ae4619da0c915c8e7d04f
fatma-12olmez <olmezfatma94@gmail.com>
2023-02-05 21:12:13 +0100

    Project

---
+++ src/main/java/proje/Runner.java
@@ -0,0 +1,17 @@
+package proje;
+
+public class Runner {
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        ArabaVeMusteriBilgisi start = new ArabaVeMusteriBilgisi();
+        start.bannerYap();
+        start.aracTalepEt();
+        start.arabaOzellikleri();
+
+        start.odemeBilgileri();
+
+
+    }
+}
